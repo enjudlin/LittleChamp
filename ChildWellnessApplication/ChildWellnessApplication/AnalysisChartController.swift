37dc1d1c6e8a2e78cc92d25c1c14d316c3a1ec32
Guest User <Guest@chens-imac.attlocal.net>
2016-11-02 21:34:05 -0400

    Reinitialize chart view elements

--- ChildWellnessApplication/ChildWellnessApplication/AnalysisChartController.swift
+++ ChildWellnessApplication/ChildWellnessApplication/AnalysisChartController.swift
@@ -8,11 +8,27 @@
 
 
 import UIKit
+import Charts
 
 class AnalysisChartController: UIViewController {
+    
+    
     var child: AppChild?
     var startDate: Date?
     var subElement: String?
+    @IBOutlet weak var timePeriodPicker: UISegmentedControl!
+    @IBOutlet weak var label0: UILabel!
+    @IBOutlet weak var switch0: UISwitch!
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var switch1: UISwitch!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var switch2: UISwitch!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var switch3: UISwitch!
+    @IBOutlet weak var label4: UILabel!
+    @IBOutlet weak var switch4: UISwitch!
+    @IBOutlet weak var lineChartView: LineChartView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
