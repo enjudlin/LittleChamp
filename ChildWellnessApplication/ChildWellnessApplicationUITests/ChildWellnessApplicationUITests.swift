140b3e2cd82904df392580c90113cda8d9eefa93
katiejackson <jackson.990@osu.edu>
2016-11-17 18:01:01 -0500

    Change "Analysis" to "View Report"

--- ChildWellnessApplication/ChildWellnessApplicationUITests/ChildWellnessApplicationUITests.swift
+++ ChildWellnessApplication/ChildWellnessApplicationUITests/ChildWellnessApplicationUITests.swift
@@ -13,8 +13,6 @@ class ChildWellnessApplicationUITests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
@@ -31,6 +29,8 @@ class ChildWellnessApplicationUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        
     }
     
 }
